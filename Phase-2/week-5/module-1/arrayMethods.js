b115cff7705a49b9e30c767ac763ea7d3f9255da
brenmeyer2245 <brenmeyer2245@gmail.com>
2022-04-19 20:25:10 -0400

    Add Quick Review

--- Phase-2/week-5/module-1/arrayMethods.js
+++ Phase-2/week-5/module-1/arrayMethods.js
@@ -8,10 +8,12 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 */
 const timesTwoPrint = (num) => {
   console.log(num * 2);
+  return num * 2;
 };
-//numbers.forEach(timesTwoPrint);
 
-// Using an anonymous function
+// numbers.forEach(timesTwoPrint);
+
+//Using an anonymous function
 // numbers.forEach(function(element, index){
 //     // our way of skipping
 //     if (index === numbers.length - 1) return
@@ -28,9 +30,9 @@ const justHellos = numbers.map(() => {
   return "Hello!";
 });
 
-const evensAndOdds = numbers.map((element) => {
-  if (element % 2) return "Odd";
-  else return "Even";
+const evensAndOdds = numbers.map((element, index, array) => {
+  if (element % 2) return element + "Odd";
+  else return element + "Even";
 });
 
 /* 
@@ -40,6 +42,8 @@ const evensAndOdds = numbers.map((element) => {
     Falsey values will be excluded
     Callback takes 2 params (element, index, array)
 */
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; ===> [1, 3, 5, 7, 9]
+
 const evensOnly = numbers.filter((element) => {
   return element % 2 === 0;
 });
@@ -52,24 +56,30 @@ const evensOnly = numbers.filter((element) => {
     For each callback, use the current element to update or affect the accumulator 
     return the accumulator each time so that it builds 
 */
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; ===> 12345678910
 
-const totalSum = numbers.reduce((accum, currentNum) => {
+const totalSum = numbers.reduce((accum, currentNum, index, array) => {
   accum += currentNum;
+  if (index && (index + 1) % 3 === 0) accum += ",";
   return accum;
-}, 0);
+}, "");
+
+console.log(totalSum);
 
 // Chaining
 // The Array functions that return new arrays can be chained
 // each one passing it's return array to the next array method for it to operate on
 
+// [2, 4,  6,  8, 10];
+
 const evensSum = numbers
   .filter((element) => {
     return element % 2 === 0;
-  })
+  }) //[2, 4,  6,  8, 10];
   .reduce((accum, num) => {
     accum += num;
     return accum;
-  }, 0);
+  }, 0); //
 
 // Extended Example
 /*
@@ -82,28 +92,32 @@ const evensSum = numbers
 
 const createListOfSquares = (state) => {
   // state has a property called board
+  //  [null, null, null, "X", "O", "X", "O", "O", "X"] ==> [DOM, DOM, DOM, DOM ]
+
   return state.board.map((square, index) => {
-    return createSquare(square, index)
+    // CREATE DOM NODES for each element in the array
+    return Square(square, index);
   });
 };
 
-const createSquare = (squareState, index) => {
+const Square = (squareValue, index) => {
     const newSquare = document.createElement("div");
     // add the value to display
-    newSquare.innerText = squareState;
+    newSquare.innerText = squareValue;
     // add a way for us to quickly lookup the cell
     newSquare.id = index;
 
     newSquare.className = "square"
-    if(squareState) newSquare.className += " taken"
-    
+    if(squareValue) newSquare.className += " taken"
+
     return newSquare
 }
 
-
 const renderSquares = (squares) => {
     const board = document.getElementById("board")
-    squares.forEach((elem) => board.appendChild(elem))
+    squares.forEach((elem) => {
+      board.appendChild(elem)
+    })
 }
 
 const state = {
@@ -111,5 +125,5 @@ const state = {
   board: [null, null, null, "X", "O", "X", "O", "O", "X"],
 };
 const squares = createListOfSquares(state);
+console.log(squares)
 renderSquares(squares)
-
