35e5f34ecd9ea3f228efd7651e056cc40d69d984
brenmeyer2245 <brenmeyer2245@gmail.com>
2022-06-04 13:33:40 -0400

    Add CreateTags recap

--- Phase-3/week-2/01-intro-to-sql/db-demo.js
+++ Phase-3/week-2/01-intro-to-sql/db-demo.js
@@ -1,8 +1,8 @@
 const { Client } = require("pg"); // imports the pg module
 
-//set up the PG client 
+//set up the PG client
 const client = new Client("postgres://localhost:5432/demo");
-//connect to the PG Client 
+//connect to the PG Client
 client.connect();
 
 /**
@@ -12,42 +12,51 @@ client.connect();
  * DELETE
  */
 
-
 /**
- * ACTION 
+ * ACTION
  *     relevant columns
  * FROM (INTO) relevant TABLE
- * CLAUSES 
- *  Ordering / Filtering / 
- *  ORDER BY / 
+ * CLAUSES
+ *  Ordering / Filtering /
+ *  ORDER BY /
  */
 
 //  client.query(`
-//  SELECT name, type 
-//   FROM pokemon 
+//  SELECT name, type
+//   FROM pokemon
 //   WHERE id = 7
 // `)
 
+client
+  .query(
+    `
+    SELECT 
+    t.name as trainername,
+    g.name as gymname
+    FROM gym_badges tg 
+    JOIN trainers t ON tg."trainerId" = t.id
+    JOIN gyms g ON g.id = tg."gymId"
+`
+  )
 
-client.query(`
-    DELETE
-     FROM pokemon 
-     WHERE id = 7
-`)
+/***
+ * name(trainers), name(gyms)
+ * ===> 
+ * gymname | trainername
+ * ---------------------
+ * name(gyms) | name(trainers)
+ */
 
-// client.query(`
-//     INSERT into 
-//         POKEMON(name, type, "trainerId")
-//         VALUES ('vulpix', 'fire', 2)
-// `)
-.then( 
-    (response) => {
-
-    const rows = response.rows
-    console.log(rows)
+  // client.query(`
+  //     INSERT into
+  //         POKEMON(name, type, "trainerId")
+  //         VALUES ('vulpix', 'fire', 2)
+  // `)
+  .then((response) => {
+    const rows = response.rows;
+    console.log(rows);
     // rows.forEach((pokemon) => {
     //     console.log(`${pokemon.name} is type ${pokemon.type}`)
     // })
     client.end();
-});
-
+  });
